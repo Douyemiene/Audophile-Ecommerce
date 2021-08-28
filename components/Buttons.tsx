02bacf61c95ce8646d4b3802c000329131e7f3c3
Douyemiene <viktormiene.vm@gmail.com>
2021-08-29 00:45:03 +0100

    feat: implement new button components

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -3,9 +3,21 @@ type ButtonOneProps = {
   extraStyle?: string;
 };
 
+export const Button = ({ text, extraStyle }: ButtonOneProps) => (
+  <div className={`${extraStyle} text-white px-10 py-1 shadow `}>{text}</div>
+);
+
 export const ButtonOne = ({ text, extraStyle }: ButtonOneProps) => (
   <div
     className={`${extraStyle} bg-primary hover:bg-primaryHover text-white px-10 py-1 shadow `}
+  >
+    {text}
+  </div>
+);
+
+export const ButtonTwo = ({ text, extraStyle }: ButtonOneProps) => (
+  <div
+    className={`${extraStyle} bg-black text-white  hover:bg-white hover:text-black hover-fontsemibold px-10 py-1 shadow `}
   >
     {text}
   </div>
